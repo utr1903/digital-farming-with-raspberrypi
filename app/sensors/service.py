0991e13e4a7a5a7f502db6404ddad80afe6e399c
utr1903 <ugurturkarslan@gmail.com>
2024-05-05 10:30:56 +0200

    Implement sensor service as an interface

---
+++ app/sensors/service.py
@@ -0,0 +1,24 @@
+
+class SensorService():
+
+    def __init__(self, sensors):
+        self.sensors = sensors
+
+    def read_sensor_values(self):
+        sensor_values = {}
+        for sensor in self.sensors:
+
+            # Get sensor data
+            sensor_name = sensor.get_sensor_name()
+            plant_name = sensor.get_plant_name()
+            temperature = sensor.read_temperature()
+            humidity = sensor.read_humidity()
+
+            # Put into map
+            sensor_values[sensor_name] = {
+                "plant": plant_name,
+                "temperature": temperature,
+                "humidity": humidity,
+            }
+
+        return sensor_values
