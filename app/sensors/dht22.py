4fff1bc6f7cba20fc195e95a7a961bd671a89cca
utr1903 <ugurturkarslan@gmail.com>
2024-05-05 10:20:09 +0200

    Implement dht22

--- app/sensors/dht22.py
+++ app/sensors/dht22.py
@@ -1,6 +1,5 @@
-# import asyncio
-# import board
-# import adafruit_dht
+import board
+import adafruit_dht
 
 from sensors import base
 
@@ -8,34 +7,29 @@ class SensorDHT22(base.SensorBase):
 
     def __init__(self, sensor_name, plant_name):
         super().__init__(sensor_name, plant_name)
-        self.test = "XXX"
-        # self.sensor = adafruit_dht.DHT22(board.D18)
+
+        self.device = adafruit_dht.DHT22(board.D18)
 
     def read_temperature(self):
-        return 20.0
+        try:
+            temperature = self.device.temperature
+            print("Temperature: {:.1f}C".format(temperature))
+
+        except RuntimeError as error:
+            print(error.args[0])
+        except Exception as error:
+            self.device.exit()
+            raise error
+        return temperature
 
     def read_humidity(self):
-        return 50.0
+        try:
+            humidity = self.device.humidity
+            print("Humidity: {:.1f}%".format(humidity))
 
-    # async def read_values(self):
-    #     while True:
-    #         try:
-    #             # Print the values to the serial port
-    #             temperature = dhtDevice.temperature
-    #             humidity = dhtDevice.humidity
-    #             print(
-    #                 "Temp: {:.1f} C / Humidity: {}% ".format(
-    #                     temperature, humidity
-    #                 )
-    #             )
-
-    #         except RuntimeError as error:
-    #             # Errors happen fairly often, DHT's are hard to read, just keep going
-    #             print(error.args[0])
-    #             await asyncio.sleep(2.0)
-    #             continue
-    #         except Exception as error:
-    #             dhtDevice.exit()
-    #             raise error
-
-    #         await asyncio.sleep(2.0)
+        except RuntimeError as error:
+            print(error.args[0])
+        except Exception as error:
+            self.device.exit()
+            raise error
+        return temperature
