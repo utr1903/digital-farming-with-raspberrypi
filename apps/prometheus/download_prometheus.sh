d2fb27f4469aebdfdda417efd760f137401e6440
utr1903 <ugurturkarslan@gmail.com>
2024-05-06 14:42:27 +0200

    Fix typos

--- apps/prometheus/download_prometheus.sh
+++ apps/prometheus/download_prometheus.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 
-wget https://github.com/prometheus/prometheus/releases/download/v2.51.2/prometheus-2.51.2.linux-amd64.tar.gz
-tar -xvf prometheus-2.51.2.linux-amd64.tar.gz
-sudo mv ./prometheus-2.51.2.linux-amd64/prometheus ./prometheus
-sudo rm -r prometheus-2.51.2.linux-amd64*
+wget https://github.com/prometheus/prometheus/releases/download/v2.51.2/prometheus-2.51.2.linux-arm64.tar.gz
+tar -xvf prometheus-2.51.2.linux-arm64.tar.gz
+sudo mv ./prometheus-2.51.2.linux-arm64/prometheus ./prometheus
+sudo rm -r prometheus-2.51.2.linux-arm64*
