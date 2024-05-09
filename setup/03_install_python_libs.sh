b4d53b812eb1e26f7c9173e5e0e2cd4a8ede45ca
Ugur Turkarslan <52721344+utr1903@users.noreply.github.com>
2024-05-09 13:41:04 +0200

    Add waitress (#19)

--- setup/03_install_python_libs.sh
+++ setup/03_install_python_libs.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/bash
 
-sudo pip3 install adafruit-circuitpython-dht==4.0.4 Flask==3.0.3 prometheus-client==0.12.0 --break-system-packages
+sudo pip3 install adafruit-circuitpython-dht==4.0.4 Flask==3.0.3 prometheus-client==0.12.0 waitress==3.0.0 --break-system-packages
 echo Y | sudo apt-get install libgpiod2
