8eb1a707e954c8d7bf1fd7fbd1e0a3c0bb4144ac
utr1903 <ugurturkarslan@gmail.com>
2024-05-12 13:48:37 +0200

    Clean up

--- apps/grafana/download_grafana.sh
+++ apps/grafana/download_grafana.sh
@@ -2,5 +2,5 @@
 
 wget https://dl.grafana.com/enterprise/release/grafana-enterprise-10.4.2.linux-arm64.tar.gz
 tar -zxvf grafana-enterprise-10.4.2.linux-arm64.tar.gz
-sudo rm -r grafana-enterprise-10.4.2.linux-arm64.tar.gz
 sudo mv ./grafana-v10.4.2 ./grafana
+sudo rm -r grafana-enterprise-10.4.2.linux-arm64.tar.gz
